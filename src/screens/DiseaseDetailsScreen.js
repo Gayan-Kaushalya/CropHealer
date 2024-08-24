572ccd167bb848d0c9a0920aee5ae3a75e05a684
Gayan Kaushalya <gaykaush@gmail.com>
2024-08-24 17:56:22 +0530

    before branching

--- src/screens/DiseaseDetailsScreen.js
+++ src/screens/DiseaseDetailsScreen.js
@@ -1,42 +1,39 @@
 import { StyleSheet, Text, View, Image, SafeAreaView, Pressable } from "react-native";
 import React from "react";
-import {FontAwesome} from "@expo/vector-icons"
+import { FontAwesome } from "@expo/vector-icons";
 
-const DiseaseDetailsScreen = ({navigation, route }) => {
-    const { disease } = route.params;
+const DiseaseDetailsScreen = ({ navigation, route }) => {
+    const { disease } = route.params || {};
 
-    console.log(disease);
+    console.log("Route Params:", route.params);
+    console.log("Disease:", disease);
 
     return (
-        <View style  = {{ backgroundColor: "#6f4e37", flex: 1 }}>
-            <SafeAreaView style={{flexDirection: "row", marginHorizontal: 16}}>
-                <Pressable style={{flex: 1}} onPress={() => navigation.goBack()}>
-                    <FontAwesome name = {"arrow-circle-left"} size={28} color="black" />
+        <View style={{ backgroundColor: "#6f4e37", flex: 1 }}>
+            <SafeAreaView style={{ flexDirection: "row", marginHorizontal: 16 }}>
+                <Pressable style={{ flex: 1 }} onPress={() => navigation.goBack()}>
+                    <FontAwesome name={"arrow-circle-left"} size={28} color="black" />
                 </Pressable>
-                
-                <FontAwesome name = {"heart-o"} size={28} color="black"/>
+                <FontAwesome name={"heart-o"} size={28} color="black" />
             </SafeAreaView>
-            <View 
-                style={{ 
-                    backgroundColor: "#fff", 
-                    flex: 1, 
+            <View
+                style={{
+                    backgroundColor: "#fff",
+                    flex: 1,
                     marginTop: 240,
-                    borderTopLeftRadius : 56,
-                    borderTopRightRadius : 56,
+                    borderTopLeftRadius: 56,
+                    borderTopRightRadius: 56,
                     alignItems: "center",
-                    }}>
-
-                 <View style = {{
-                  //  backgroundColor: "black",
+                }}>
+                <View style={{
                     height: 300,
                     width: 300,
                     position: "absolute",
                     top: -150,
-                
-                    }}>      
-                <Image source={require("../../assets/diseases/anthracnose.png")} style={{ height: "100%", width: "100%", resizeMode: "contain" }} /> 
+                }}>
+                    <Image source={require("../../assets/diseases/anthracnose.png")} style={{ height: "100%", width: "100%", resizeMode: "contain" }} />
+                </View>
             </View>
-        </View>
         </View>
     );
 };
