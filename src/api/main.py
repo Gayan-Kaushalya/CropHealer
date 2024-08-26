006d2f1be74b5299f44e482cd7ccc2bd91f046ad
Gayan Kaushalya <gaykaush@gmail.com>
2024-08-26 17:28:11 +0530

    minor edit

--- src/api/main.py
+++ src/api/main.py
@@ -8,6 +8,9 @@ import tensorflow as tf
 
 app = FastAPI()
 
+MODEL = tf.keras.models.load_model("../models/pepper")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
 @app.get("/ping")
 async def ping():
     return "pong"
@@ -20,9 +23,14 @@ def read_file_as_image(file) -> np.ndarray:
 
 @app.post("/predict")
 async def predict(file : UploadFile = File(...)):
-  #  bytes = await file.read()
-    image = read_file_as_image(await file.read())
-    return image
+    
+ # bytes = await file.read()
+  image = read_file_as_image(await file.read())
+  img_batch = np.expand_dims(image, 0)
+  prediction = MODEL.predict(img_batch)
+  
+  pass
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="localhost", port=8001)
+    
