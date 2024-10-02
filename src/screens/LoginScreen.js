78202598690d259c11969ab6fd1c6748fb6f91d0
Gayan Kaushalya <gaykaush@gmail.com>
2024-10-02 13:34:34 +0530

    UI Setup over

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { View, Text, Button, StyleSheet, KeyboardAvoidingView, TextInput, Platform } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { View, Text, Button, StyleSheet, KeyboardAvoidingView, TextInput, Platform, Touchable } from "react-native";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
@@ -25,6 +26,25 @@ const LoginScreen = () => {
           secureTextEntry
         />
       </View>
+
+      <View
+        style = {styles.buttonContainer}
+      >
+            <TouchableOpacity
+                onPress = {() => {}}
+                style = {styles.button}
+            >
+                <Text style = {styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                onPress = {() => {}}
+                style = {[styles.button, styles.buttonOutline]}
+            >
+                <Text style = {styles.buttonOutlineText}>Register</Text>
+            </TouchableOpacity>
+            
+      </View>
     </KeyboardAvoidingView>
   );
 };
@@ -45,9 +65,49 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
-    marginBottom: 12,
+    marginBottom: 8,
     paddingHorizontal: 8,
     borderRadius: 4,
     backgroundColor: 'white',
   },
+
+  buttonContainer: {
+    width: '60%',
+    alignItems: 'center',
+    justifyContent: 'center',
+   // color: 'blue',
+   marginTop: 10,
+  },
+
+  button: {
+    backgroundColor: '#6495ed',
+    padding: 10,
+    borderRadius: 5,
+    width: '80%',
+    alignItems: 'center',
+    marginTop: 2,
+    marginBottom: 5,
+    borderRadius: 10,
+  },
+
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: 'blue',
+    borderWidth: 2,
+
+  },
+
+  buttonText: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+
+  buttonOutlineText: {
+    color: 'blue',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  
 });
