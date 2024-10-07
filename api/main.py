ae16aadd7f0d87cff29fa4da88a8020d87661db7
Gayan Kaushalya <gaykaush@gmail.com>
2024-10-07 16:04:59 +0530

    Tested Sumuditha's new plant model. There is a problem in picture size.

--- api/main.py
+++ api/main.py
@@ -21,29 +21,36 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+#plant_model_path = "models/plantNew.h5"
+
 plant_model_path = "models/plantModel.h5"
 tomato_model_path = "models/tomatoModel.h5"
 pepper_model_path = "models/pepperModel.h5"
 potato_model_path = "models/potatoModel.h5"
+tea_model_path = "models/teaModel.h5"
 
 if os.path.exists(plant_model_path):
     MODEL = tf.keras.models.load_model(plant_model_path)
 else:
     raise FileNotFoundError(f"Model file not found at path: {plant_model_path}")
 
+#CLASS_NAMES = ["Tea", "Grapes", "Bean", "Eggplant", "Pepper", "Corn", "Rice", "Potato", "Apple", "Tomato"]
+
+
 CLASS_NAMES = ["Pepper","Potato", "Tomato"]
 POTATO_CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
-TOMATO_CLASS_NAMES = ['Bacterial_spot',
- 'Early_blight',
- 'Late_blight',
- 'Leaf_Mold',
- 'Septoria_leaf_spot',
- 'Spider_mites Two-spotted_spider_mite',
- 'Target_Spot',
- 'Tomato_Yellow_Leaf_Curl_Virus',
- 'Tomato_mosaic_virus',
- 'healthy']
+TOMATO_CLASS_NAMES = ['Bacterial Spot',
+ 'Early Blight',
+ 'Late Blight',
+ 'Leaf Mold',
+ 'Septoria Leaf Spot',
+ 'Two-spotted Spider Mite',
+ 'Target Spot',
+ 'Tomato Yellow Leaf Curl Virus',
+ 'Tomato Mosaic Virus',
+ 'Healthy']
 PEPPER_CLASS_NAMES = ['Bacterial_spot', 'Healthy']
+TEA_CLASS_NAMES = ['Anthracnose', 'Algal Leaf', 'Bird Eye Spot', 'Brown Blight', 'Gray Light', 'Healthy', 'Red Leaf Spot', 'White Spot']
 
 
 @app.get("/ping")
@@ -75,17 +82,39 @@ async def predict(image_data:ImageData):
     if crop == "Potato":
         next_model = tf.keras.models.load_model(potato_model_path)
         prediction = next_model.predict(img_batch)
-        return {"crop": crop, "class": POTATO_CLASS_NAMES[np.argmax(prediction[0])], "confidence": float(np.max(prediction[0]))}
+        return {"crop": crop, "class": POTATO_CLASS_NAMES[np.argmax(prediction[0])], "confidence": str(round(float(np.max(prediction[0]))*100, 2))+"%"}
     
     if crop == "Tomato":
         next_model = tf.keras.models.load_model(tomato_model_path)
         prediction = next_model.predict(img_batch)
-        return {"crop": crop, "class": TOMATO_CLASS_NAMES[np.argmax(prediction[0])], "confidence": float(np.max(prediction[0]))}
+        return {"crop": crop, "class": TOMATO_CLASS_NAMES[np.argmax(prediction[0])], "confidence": str(round(float(np.max(prediction[0]))*100, 2))+"%"}
     
     if crop == "Pepper":
         next_model = tf.keras.models.load_model(pepper_model_path)
         prediction = next_model.predict(img_batch)
-        return {"crop": crop, "class": PEPPER_CLASS_NAMES[np.argmax(prediction[0])], "confidence": float(np.max(prediction[0]))}
+        return {"crop": crop, "class": PEPPER_CLASS_NAMES[np.argmax(prediction[0])], "confidence": str(round(float(np.max(prediction[0]))*100, 2))+"%"}
+    
+    if crop == "Tea":
+        return {"crop": crop, "class": "Tea", "confidence": "100%"}
+    
+    if crop == "Grapes":
+        return {"crop": crop, "class": "Grapes", "confidence": "100%"}
+    
+    if crop == "Bean":
+        return {"crop": crop, "class": "Bean", "confidence": "100%"}
+    
+    if crop == "Eggplant":
+        return {"crop": crop, "class": "Eggplant", "confidence": "100%"}
+    
+    if crop == "Corn":
+        return {"crop": crop, "class": "Corn", "confidence": "100%"}
+    
+    if crop == "Rice":
+        return {"crop": crop, "class": "Rice", "confidence": "100%"}
+    
+    if crop == "Apple":
+        return {"crop": crop, "class": "Apple", "confidence": "100%"}
+    
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8001)
